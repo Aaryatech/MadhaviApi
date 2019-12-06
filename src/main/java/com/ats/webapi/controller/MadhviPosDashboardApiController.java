fd6a86f7bd20346168300f9a62a373080791685f
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2019-12-06 17:24:58 +0530

    //harsha

--- src/main/java/com/ats/webapi/controller/MadhviPosDashboardApiController.java
+++ src/main/java/com/ats/webapi/controller/MadhviPosDashboardApiController.java
@@ -1,5 +1,8 @@
 package com.ats.webapi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ats.webapi.model.posdashboard.BillHeaderDashCount;
 import com.ats.webapi.model.posdashboard.BillTransactionDetailDashCount;
+import com.ats.webapi.model.posdashboard.CategorywiseItemSell;
+import com.ats.webapi.model.posdashboard.CategorywiseSell;
 import com.ats.webapi.model.posdashboard.PosDashCounts;
 import com.ats.webapi.model.posdashboard.SellBillHeaderDashCounts;
 import com.ats.webapi.repo.posdashboard.BillHeaderDashCountRepo;
 import com.ats.webapi.repo.posdashboard.BillTransactionDetailDashCountRepo;
+import com.ats.webapi.repo.posdashboard.CategorywiseItemSellRepo;
+import com.ats.webapi.repo.posdashboard.CategorywiseSellRepo;
 import com.ats.webapi.repo.posdashboard.SellBillHeaderDashCountsRepo;
 
 @RestController
@@ -35,11 +42,21 @@ public class MadhviPosDashboardApiController {
 
 		SellBillHeaderDashCounts headcount = new SellBillHeaderDashCounts();
 		BillTransactionDetailDashCount tranCount = new BillTransactionDetailDashCount();
-		BillHeaderDashCount billCount = new BillHeaderDashCount();
+		BillHeaderDashCount billCountch = new BillHeaderDashCount();
+		BillHeaderDashCount billCountpur = new BillHeaderDashCount();
+		
+		System.err.println( "DashBoardReporApi data is " + fromDate+toDate+frId);
 		try {
 			headcount = sellBillHeaderDashCountsRepo.getDataFordash(fromDate, toDate, frId);
 			tranCount = billTransactionDetailDashCountRepo.getD1ataFordash(fromDate, toDate, frId);
-			billCount = billHeaderDashCountRepo.getD1ataFordash2(fromDate, toDate, frId);
+			billCountch = billHeaderDashCountRepo.getD1ataFordash2Ch(fromDate, toDate, frId);
+			billCountpur = billHeaderDashCountRepo.getD1ataFordash2pur(fromDate, toDate, frId);
+			
+			//System.err.println( "DashBoardReporApi /headcount" + headcount.toString());
+			
+			//System.err.println( "DashBoardReporApi /tranCount" + tranCount.toString());
+			//System.err.println( "DashBoardReporApi /billCountch" + billCountch.toString());
+			//System.err.println( "DashBoardReporApi /billCountpur" + billCountpur.toString());
 
 			crnReport.setAdvanceAmt(headcount.getAdvanceAmt());
 			crnReport.setCardAmt(tranCount.getCardAmt());
@@ -50,10 +67,68 @@ public class MadhviPosDashboardApiController {
 			crnReport.setNoOfBillGenerated(headcount.getNoBillGen());
 			crnReport.setSaleAmt(headcount.getSellAmt());
 
-			crnReport.setExpenseAmt(billCount.getChAmt());
+			crnReport.setExpenseAmt(billCountch.getChAmt());
 			crnReport.setProfitAmt(headcount.getProfitAmt());
-			crnReport.setPurchaseAmt(billCount.getPurchaeAmt());
+			crnReport.setPurchaseAmt(billCountpur.getPurchaeAmt());
+			
+			System.err.println( "DashBoardReporApi /getCredNoteReport" + crnReport.toString());
 
+		} catch (Exception e) {
+
+			System.err.println("Exception in DashBoardReporApi /getCredNoteReport" + e.getMessage());
+
+			e.printStackTrace();
+		}
+
+		return crnReport;
+	}
+	
+	@Autowired
+	CategorywiseSellRepo categorywiseSellRepo;
+	
+	
+	@RequestMapping(value = { "/getCatwiseSell" }, method = RequestMethod.POST)
+	public @ResponseBody List<CategorywiseSell> getCatwiseSell(@RequestParam("fromDate") String fromDate,
+			@RequestParam("toDate") String toDate, @RequestParam("frId") int frId) {
+		
+		List<CategorywiseSell> crnReport = new  ArrayList<CategorywiseSell>();
+
+ 		 
+		try {
+			
+			crnReport = categorywiseSellRepo.getCategorywiseSell(fromDate, toDate, frId);
+			 
+		} catch (Exception e) {
+
+			System.err.println("Exception in DashBoardReporApi /getCredNoteReport" + e.getMessage());
+
+			e.printStackTrace();
+		}
+
+		return crnReport;
+	}
+	
+	@Autowired
+	CategorywiseItemSellRepo categorywiseItemSellRepo;
+	
+	
+	@RequestMapping(value = { "/getCatwiseItemSell" }, method = RequestMethod.POST)
+	public @ResponseBody List<CategorywiseItemSell> getCatwiseItemSell(@RequestParam("fromDate") String fromDate,
+			@RequestParam("toDate") String toDate, @RequestParam("frId") int frId,@RequestParam("catId") int catId,@RequestParam("flag") int flag) {
+		
+		List<CategorywiseItemSell> crnReport = new ArrayList<CategorywiseItemSell>();
+
+ 		 
+		try {
+			if(flag==1) {
+				crnReport = categorywiseItemSellRepo.getCategorywiseItemSellDesc(fromDate, toDate, frId,catId);
+			}else if(flag==2) {
+				crnReport = categorywiseItemSellRepo.getCategorywiseItemSellAsc(fromDate, toDate, frId,catId);
+			}else {
+				crnReport = categorywiseItemSellRepo.getCategorywiseItemSellAll(fromDate, toDate, frId,catId);
+			}
+			
+			 
 		} catch (Exception e) {
 
 			System.err.println("Exception in DashBoardReporApi /getCredNoteReport" + e.getMessage());
--- src/main/java/com/ats/webapi/repo/posdashboard/BillHeaderDashCountRepo.java
+++ src/main/java/com/ats/webapi/repo/posdashboard/BillHeaderDashCountRepo.java
@@ -10,10 +10,21 @@ public interface BillHeaderDashCountRepo extends JpaRepository<BillHeaderDashCou
 
 	
 	@Query(value = "SELECT\n" + 
-			"  UUID() AS uid,SUM( m_expense.ch_amt) as ch_amt,SUM(t_bill_header.grand_total) as purchae_amt\n" + 
+			"    UUID() AS uid, SUM(m_expense.ch_amt) AS ch_amt,\n" + 
+			"    '0' AS purchae_amt\n" + 
 			"FROM\n" + 
-			"   m_expense,t_bill_header\n" + 
+			"    m_expense\n" + 
 			"WHERE\n" + 
-			"    m_expense.del_status = 0 AND m_expense.fr_id =:frId AND m_expense.exp_date BETWEEN :fromDate AND :toDate AND t_bill_header.del_status=0  AND t_bill_header.fr_id=:frId AND t_bill_header.bill_date BETWEEN :fromDate AND :toDate", nativeQuery = true)
-	BillHeaderDashCount getD1ataFordash2(@Param("fromDate") String fromDate, @Param("toDate") String toDate,@Param("frId") int  frId);
+			"    m_expense.del_status = 0 AND m_expense.fr_id =:frId AND m_expense.exp_date BETWEEN :fromDate AND :toDate ", nativeQuery = true)
+	BillHeaderDashCount getD1ataFordash2Ch(@Param("fromDate") String fromDate, @Param("toDate") String toDate,@Param("frId") int  frId);
+	
+	
+	@Query(value = "SELECT\n" + 
+			"    UUID() AS uid, '0' AS ch_amt, SUM(t_bill_header.grand_total) AS purchae_amt\n" + 
+			"FROM\n" + 
+			"    t_bill_header,\n" + 
+			"    m_franchisee\n" + 
+			"WHERE\n" + 
+			"    t_bill_header.del_status = 0 AND t_bill_header.fr_id =:frId AND t_bill_header.bill_date BETWEEN :fromDate AND :toDate", nativeQuery = true)
+	BillHeaderDashCount getD1ataFordash2pur(@Param("fromDate") String fromDate, @Param("toDate") String toDate,@Param("frId") int  frId);
 }
