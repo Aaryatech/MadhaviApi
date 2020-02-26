2fd0f68ab4829d97a2fa7ea1b5d2b9191caecb94
maddy <maddy@maddy-Inspiron-3541>
2020-02-26 10:43:53 +0530

    //maddy

--- src/main/java/com/ats/webapi/repository/ItemWiseDetailRepository.java
+++ src/main/java/com/ats/webapi/repository/ItemWiseDetailRepository.java
@@ -12,10 +12,16 @@ import com.ats.webapi.model.ItemWiseDetail;
 @Repository
 public interface ItemWiseDetailRepository extends JpaRepository<ItemWiseDetail, Long>{
 
-	@Query(value="SELECT t_bill_detail.bill_detail_no, t_bill_header.invoice_no as bill_no ,t_bill_header.bill_date, t_bill_detail.item_id ,  m_item.item_name ,t_bill_detail.rate,COALESCE(SUM(bill_qty),0) AS qty, COALESCE(SUM(t_bill_detail.grand_total),0) AS total ,t_bill_detail.grn_type,t_bill_detail.expiry_date, COALESCE(SUM(t_bill_detail.remark),0) AS disc_amt FROM t_bill_detail , m_item, t_bill_header WHERE  m_item.id= t_bill_detail.item_id AND t_bill_detail.bill_no IN(SELECT bill_no FROM t_bill_header WHERE bill_date BETWEEN :fromDate AND :toDate AND fr_id=:frId) AND t_bill_detail.bill_no=t_bill_header.bill_no AND t_bill_detail.cat_id=:catId and t_bill_header.del_status=0 and t_bill_detail.del_status=0 GROUP BY t_bill_detail.item_id, t_bill_detail.bill_no",nativeQuery=true)
+	@Query(value="SELECT t_bill_detail.bill_detail_no, t_bill_header.invoice_no as bill_no ,t_bill_header.bill_date, t_bill_detail.item_id ,  m_item.item_name ,t_bill_detail.rate,COALESCE(SUM(bill_qty),0) AS qty, COALESCE(SUM(t_bill_detail.grand_total),0) AS total ,t_bill_detail.grn_type,t_bill_detail.expiry_date, COALESCE(SUM(t_bill_detail.remark),0) AS disc_amt FROM t_bill_detail , m_item, t_bill_header WHERE  m_item.id= t_bill_detail.item_id AND t_bill_detail.bill_no IN(SELECT bill_no FROM t_bill_header WHERE bill_date BETWEEN :fromDate AND :toDate AND fr_id=:frId) AND t_bill_detail.bill_no=t_bill_header.bill_no AND t_bill_detail.cat_id=:catId and t_bill_header.del_status=0 and t_bill_detail.del_status=0 GROUP BY t_bill_detail.item_id, t_bill_detail.bill_no ORDER BY t_bill_header.invoice_no DESC",nativeQuery=true)
 	List<ItemWiseDetail> findItemWiseDetailReport(@Param("frId")int frId,@Param("catId")int catId,@Param("fromDate")String fromDate,@Param("toDate") String toDate);
 
-	@Query(value="SELECT t_bill_detail.bill_detail_no, t_bill_header.invoice_no as bill_no ,t_bill_header.bill_date, t_bill_detail.item_id ,  m_sp_cake.sp_name AS item_name ,t_bill_detail.rate,COALESCE(SUM(bill_qty),0) AS qty, COALESCE(SUM(t_bill_detail.grand_total),0) AS total ,t_bill_detail.grn_type,t_bill_detail.expiry_date, COALESCE(SUM(t_bill_detail.remark),0) AS disc_amt FROM t_bill_detail , m_sp_cake, t_bill_header WHERE  m_sp_cake.sp_id= t_bill_detail.item_id AND t_bill_detail.bill_no IN(SELECT bill_no FROM t_bill_header WHERE bill_date BETWEEN :fromDate AND :toDate AND fr_id=:frId) AND t_bill_detail.bill_no=t_bill_header.bill_no AND t_bill_detail.cat_id=:catId and t_bill_header.del_status=0 and t_bill_detail.del_status=0 GROUP BY t_bill_detail.item_id, t_bill_detail.bill_no",nativeQuery=true)
+	@Query(value="SELECT t_bill_detail.bill_detail_no, t_bill_header.invoice_no as bill_no ,t_bill_header.bill_date, t_bill_detail.item_id ,  "
+			+ "m_sp_cake.sp_name AS item_name ,t_bill_detail.rate,COALESCE(SUM(bill_qty),0) AS qty, COALESCE(SUM(t_bill_detail.grand_total),0) AS total ,"
+			+ "t_bill_detail.grn_type,t_bill_detail.expiry_date, COALESCE(SUM(t_bill_detail.remark),0) AS disc_amt FROM t_bill_detail , m_sp_cake, "
+			+ "t_bill_header WHERE  m_sp_cake.sp_id= t_bill_detail.item_id AND t_bill_detail.bill_no IN(SELECT bill_no FROM t_bill_header "
+			+ "WHERE bill_date BETWEEN :fromDate AND :toDate AND fr_id=:frId) AND t_bill_detail.bill_no=t_bill_header.bill_no AND "
+			+ "t_bill_detail.cat_id=:catId and t_bill_header.del_status=0 and t_bill_detail.del_status=0 "
+			+ "GROUP BY t_bill_detail.item_id, t_bill_detail.bill_no ORDER BY t_bill_header.invoice_no DESC",nativeQuery=true)
 	List<ItemWiseDetail> findSpecialCakeWiseDetailReport(@Param("frId")int frId,@Param("catId") int catId,@Param("fromDate") String fromDate,@Param("toDate") String toDate);
 
 	@Query(value="SELECT\n" + 
@@ -51,7 +57,7 @@ public interface ItemWiseDetailRepository extends JpaRepository<ItemWiseDetail, 
 			"        and t_bill_detail.del_status=0 \n" + 
 			"    GROUP BY\n" + 
 			"        t_bill_detail.item_id,\n" + 
-			"        t_bill_detail.bill_no",nativeQuery=true)
+			"        t_bill_detail.bill_no ORDER BY t_bill_header.invoice_no DESC",nativeQuery=true)
 	List<ItemWiseDetail> getItemWiseDetailReportByItemIds(@Param("frId")int frId,@Param("itemIds") List<Integer> itemIds,@Param("fromDate") String fromDate,
 			@Param("toDate") String toDate);
 
@@ -90,7 +96,7 @@ public interface ItemWiseDetailRepository extends JpaRepository<ItemWiseDetail, 
 			"        and t_bill_detail.del_status=0      \n" + 
 			"    GROUP BY\n" + 
 			"        t_bill_detail.item_id,\n" + 
-			"        t_bill_detail.bill_no",nativeQuery=true)
+			"        t_bill_detail.bill_no ORDER BY t_bill_header.invoice_no DESC",nativeQuery=true)
 	List<ItemWiseDetail> findSpecialCakeWiseDetailReportByItemIds(@Param("frId")int frId,@Param("catId")int catId,@Param("itemIds") List<Integer> itemIds,@Param("fromDate") String fromDate,
 			@Param("toDate") String toDate);
 
@@ -129,7 +135,7 @@ public interface ItemWiseDetailRepository extends JpaRepository<ItemWiseDetail, 
 			"        and t_bill_detail.del_status=0 \n" + 
 			"    GROUP BY\n" + 
 			"        t_bill_detail.item_id,\n" + 
-			"        t_bill_detail.bill_no",nativeQuery=true)
+			"        t_bill_detail.bill_no ORDER BY t_bill_header.invoice_no DESC",nativeQuery=true)
 	List<ItemWiseDetail> findItemWiseDetailReportsubCatwise(@Param("frId")int frId,@Param("catId") int catId,@Param("fromDate") String fromDate,@Param("toDate") String toDate);
 
 	
@@ -167,7 +173,7 @@ public interface ItemWiseDetailRepository extends JpaRepository<ItemWiseDetail, 
 			"        and t_bill_detail.del_status=0 \n" + 
 			"    GROUP BY\n" + 
 			"        t_bill_detail.item_id,\n" + 
-			"        t_bill_detail.bill_no",nativeQuery=true)
+			"        t_bill_detail.bill_no ORDER BY t_bill_header.invoice_no DESC",nativeQuery=true)
 	List<ItemWiseDetail> findAllSpecialCakeWiseDetailReport(@Param("frId") int frId, @Param("fromDate") String fromDate, @Param("toDate") String toDate);
 	
 }
