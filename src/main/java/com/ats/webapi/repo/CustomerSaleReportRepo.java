f8b030dd0f5139d0d0f325f61ead4c2ea386e48c
ats_lap11 <ats_lap11@HP-NotebookAtslap11>
2020-11-24 12:19:30 +0530

    Change In Query Order By payableAmt DESC --Akhilesh

--- src/main/java/com/ats/webapi/repo/CustomerSaleReportRepo.java
+++ src/main/java/com/ats/webapi/repo/CustomerSaleReportRepo.java
@@ -13,7 +13,7 @@ public interface CustomerSaleReportRepo extends JpaRepository<CustomerSaleReport
 	@Query(value="SELECT c.*,SUM(h.payable_amt) as payable_amt,SUM(h.discount_amt) AS disc_amt,SUM(h.ext_float3) AS wallet,"
 			+ "SUM(h.ext_float2) AS extra_ch FROM m_customer c,t_sell_bill_header h WHERE c.del_status=0 AND "
 			+ "c.added_from_type IN(:type) AND h.cust_id=c.cust_id AND h.del_status=0 AND "
-			+ "h.bill_date BETWEEN :fromDate AND :toDate AND h.fr_id IN(:frId) GROUP BY h.cust_id ORDER BY c.cust_name",nativeQuery=true)
+			+ "h.bill_date BETWEEN :fromDate AND :toDate AND h.fr_id IN(:frId) GROUP BY h.cust_id ORDER BY payable_amt desc",nativeQuery=true)
 	List<CustomerSaleReport> getCustByAddedTypeAndFr(@Param("type") List<Integer> type,@Param("frId") List<Integer> frId, @Param("fromDate") String fromDate, @Param("toDate") String toDate);
 	
 	
